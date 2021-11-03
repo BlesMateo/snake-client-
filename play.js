74f2d5bd7dddedcdeb358e4f2e1cfef5ad5e32ab
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-03 15:36:54 -0400

    addedname to the client file

--- play.js
+++ play.js
@@ -10,6 +10,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", (connect)=>{
+    console.log("Successfully connected to the server");
+
+  });
+
   conn.on("data", (data) => {
     console.log("Idle:", data);
   });
