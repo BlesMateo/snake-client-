ac3aded638dfce0f70cec1ddadf0559ae6a15c71
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-03 18:30:54 -0400

    Added movement commands and hard-coded the move-up message

--- client.js
+++ client.js
@@ -11,4 +11,16 @@ const {connect} = require('./play');
   // interpret incoming data as text
   conn.on('connect', ()=> {
     conn.write("Name: BAM");
-  })
+
+      //setTimeout(() => {conn.write('Move: up')}, 500);
+      setTimeout(() => {conn.write('Move: left')}, 100);
+      setTimeout(() => {conn.write('Move: down')}, 150);
+      setTimeout(() => {conn.write('Move: left')}, 200);
+      //setTimeout(() => {conn.write('Move: up')}, 250);
+      setTimeout(() => {conn.write('Move: left')}, 300);
+      setTimeout(() => {conn.write('Move: down')}, 350);
+      setTimeout(() => {conn.write('Move: left')}, 400);
+
+  });
+
+  return conn;
