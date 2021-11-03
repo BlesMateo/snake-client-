b0943e719b4f9f32a9153df54dacaed1286f1bd5
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-03 14:53:52 -0400

    Added module export to the play file

--- play.js
+++ play.js
@@ -19,3 +19,18 @@ const connect = function () {
 
 console.log("Connecting ...");
 connect();
+
+module.exports = {connect}
+/*
+const myNumber = 42;
+const myString = "hello";
+const myFunction = () => {
+  // myFunction's code
+};
+
+module.exports = {
+  myNumber, // stores 42 as myNumber
+  myString, // stores "hello" as myString
+  myFunction, // stores the function as myFunction
+};
+*/
