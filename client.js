4a6c7fa767ee7723748c191f86a77d9090fe8eef
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-03 21:37:16 -0400

    Moved IP and PORT number to the connect function

--- client.js
+++ client.js
@@ -1,26 +1,37 @@
-const net = require('net');
+const { IP, PORT } = require("./constants");
+const net = require("net");
 
-const {connect} = require('./play');
-
-
+// establishes a connection with the game server
+const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
-  conn.on('connect', ()=> {
-    conn.write("Name: BAM");
+  conn.setEncoding("utf8");
 
-      //setTimeout(() => {conn.write('Move: up')}, 500);
-      setTimeout(() => {conn.write('Move: left')}, 100);
-      setTimeout(() => {conn.write('Move: down')}, 150);
-      setTimeout(() => {conn.write('Move: left')}, 200);
-      //setTimeout(() => {conn.write('Move: up')}, 250);
-      setTimeout(() => {conn.write('Move: left')}, 300);
-      setTimeout(() => {conn.write('Move: down')}, 350);
-      setTimeout(() => {conn.write('Move: left')}, 400);
+  conn.on("data", (data) => {
+    console.log("Idle:", data);
 
   });
+  conn.on('connect', ()=> {
+    console.log('Successfully connected to game server!')
+    conn.write("Name: BAM");
 
+    conn.write("Say: Hello people staying up pass midnight!")
+
+      //setTimeout(() => {conn.write('Move: up')}, 50);
+      //setTimeout(() => {conn.write('Move: left')}, 100);
+      //setTimeout(() => {conn.write('Move: down')}, 150);
+      //setTimeout(() => {conn.write('Move: left')}, 200);
+      //setTimeout(() => {conn.write('Move: up')}, 250);
+      //setTimeout(() => {conn.write('Move: left')}, 300);
+      //setTimeout(() => {conn.write('Move: down')}, 350);
+      //setTimeout(() => {conn.write('Move: left')}, 400);
+
+  });
   return conn;
+};
+
+module.exports = {connect};
