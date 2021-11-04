501a2f0b2d9e6e2e74c2cb3f6b063493deac1831
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-03 21:36:17 -0400

    refactored the code to contain connect and setupInput

--- play.js
+++ play.js
@@ -1,55 +1,6 @@
-const net = require("net");
-
-// establishes a connection with the game server
-  const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-  };
-
-  const handleUserInput = function () {
-
-  };
-
-  const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", (connect)=>{
-    console.log("Successfully connected to the server");
-
-  });
-
-  conn.on("data", (data) => {
-    console.log("Idle:", data);
-  });
-
-    return conn;
-};
+const {connect} = require('./client.js')
+const {setupInput} = require('./input.js')
 
 console.log("Connecting ...");
-connect();
-setupInput();
 
-module.exports = {connect}
-/*
-const myNumber = 42;
-const myString = "hello";
-const myFunction = () => {
-  // myFunction's code
-};
-
-module.exports = {
-  myNumber, // stores 42 as myNumber
-  myString, // stores "hello" as myString
-  myFunction, // stores the function as myFunction
-};
-*/
+setupInput(connect());
